4ff56f20ef0886e5a5313631e7655540a144161f
Willian Antunes <willian.lima.antunes@gmail.com>
2019-07-13 10:03:40 -0300

    To be clearer
    The logic to concatenate "publisher" is handled internally

--- django_stomp/publisher.py
+++ django_stomp/publisher.py
@@ -1,4 +1,3 @@
-import uuid
 from typing import Optional
 
 from django.conf import settings
@@ -7,17 +6,15 @@ from django_stomp.services.producer import Publisher
 
 
 def build_publisher(client_id: Optional[str] = None) -> Publisher:
-
-    client_id = client_id if client_id else ""
-    client_id = f"{client_id}-{uuid.uuid4().hex}-publisher"
-
-    connection_params = {
-        "use_ssl": settings.STOMP_USE_SSL,
+    required_params = {
         "host": settings.STOMP_SERVER_HOST,
         "port": int(settings.STOMP_SERVER_PORT),
         "username": settings.STOMP_SERVER_USER,
         "password": settings.STOMP_SERVER_PASSWORD,
-        "client_id": client_id,
     }
+
+    extra_params = {"use_ssl": settings.STOMP_USE_SSL, "client_id": client_id}
+
+    connection_params = {k: v for k, v in {**required_params, **extra_params}.items() if v}
 
     return producer.build_publisher(**connection_params)
