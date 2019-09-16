9903a320d98bace8dd161cbbba5ec570d031670e
Willian Antunes <willian.lima.antunes@gmail.com>
2019-09-16 14:00:21 -0300

    Refactored pubsub Django custom command

--- django_stomp/management/commands/pubsub.py
+++ django_stomp/management/commands/pubsub.py
@@ -6,8 +6,18 @@ class Command(BaseCommand):
     help = "Start internal PUB/SUB logic"
 
     def add_arguments(self, parser):
-        parser.add_argument("consume_queue", type=str)
-        parser.add_argument("callback", type=str)
+        parser.add_argument("source_destination", type=str, help="Source destination used to consume messages")
+        parser.add_argument(
+            "callback_function",
+            type=str,
+            help="Entire module path with its function to process messages from source destination",
+        )
 
     def handle(self, *args, **options):
-        start_processing(options.get("consume_queue"), options.get("callback"))
+        self.stdout.write(f"Provided parameters: {options}")
+
+        source_destination = options.get("source_destination")
+        callback_function = options.get("callback_function")
+
+        self.stdout.write("Calling internal service to consume messages")
+        start_processing(source_destination, callback_function)
