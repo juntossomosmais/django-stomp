89219e21b57ecd24109f7835128fda624b2aee81
Willian Antunes <willian.lima.antunes@gmail.com>
2019-12-17 10:01:38 -0300

    New major version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django_stomp",
-    version="0.0.23",
+    version="1.0.0",
     description="A simple implementation of STOMP with Django",
     long_description=README,
     long_description_content_type="text/markdown",
