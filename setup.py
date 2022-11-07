7328db4ae5a2365d0291d2f8564a2d1f79a1667c
Alexandre Xavier <ale.bxsantos@gmail.com>
2022-11-07 09:22:58 -0300

    chore: updated version on setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stomp",
-    version="5.1.0",
+    version="5.1.1",
     description="A simple implementation of STOMP with Django",
     long_description=README,
     long_description_content_type="text/markdown",
